012fb97e3b0dc5284e7c56b11fe275ad0179254c
Louis Catteau <louiscatteauo@gmail.com>
2024-05-21 17:15:47 +0200

    autorisation de localisation

--- screens/FestivalScreen.js
+++ screens/FestivalScreen.js
@@ -62,6 +62,9 @@ export default function FestivalScreen({ navigation, route: { params: { ...props
   const handleShare = () => {
 
   }
+  if (!fontsLoaded) {
+    return <Text></Text> ;
+  }
 
   return (
     <View style={styles.container}>
